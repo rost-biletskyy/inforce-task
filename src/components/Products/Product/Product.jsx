5c9aadcca225520d9ad022f4e87a61702e920e55
Ростик <bilka2001@gmail.com>
2021-10-24 17:25:56 +0300

    fix

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Product = ({ product, onAddToCart }) => {
+  const classes = useStyles();
+
+  const handleAddToCart = () => onAddToCart(product.id, 1);
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            ${product.price.formatted}
+          </Typography>
+        </div>
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}> + </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
+
